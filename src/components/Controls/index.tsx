711b88d195106c7dda2333084e94f06b5b68fd90
Malik Elbadri <elbadrimalik@gmail.com>
2024-01-13 13:59:01 +0300

    Created root styling module

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -10,6 +10,14 @@ export default function Controls({ onPlayChange, isPlaying }: Props) {
       <button title="Play" onClick={onPlayChange} type="button">
         <PlayIcon isPlaying={isPlaying} />
       </button>
+      <div className={styles.timerContainer}></div>
+      <button
+        className={styles.fullScreenButton}
+        type="button"
+        title="Full screen"
+      >
+        <FullScreenIcon />
+      </button>
     </div>
   );
 }
@@ -19,4 +27,8 @@ type PlayProps = {
 };
 function PlayIcon({ isPlaying = false }: PlayProps) {
   return <div className={styles.PlayIcon} data-playing={isPlaying} />;
+}
+
+function FullScreenIcon() {
+  return <div className={styles.fullScreenIcon}></div>;
 }
--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,15 +1,10 @@
+import React from "react";
 import styles from "./index.module.scss";
 
 type Props = {
   percentage: number;
 };
 export default function Progress({ percentage }: Props) {
-  return (
-    <div
-      className={styles.progress}
-      style={{
-        width: `${percentage}%`,
-      }}
-    />
-  );
+  const style: React.CSSProperties = { width: `${percentage}%` };
+  return <div className={styles.container} style={style} />;
 }
