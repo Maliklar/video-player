ef84a7a6e847d3a033ce877c18dc613a6fc2d6d7
Malik Elbadri <elbadrimalik@gmail.com>
2024-01-13 14:52:51 +0300

    Updated drag styling

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -3,8 +3,21 @@ import styles from "./index.module.scss";
 
 type Props = {
   percentage: number;
+  onProgressChange: 
 };
 export default function Progress({ percentage }: Props) {
   const style: React.CSSProperties = { width: `${percentage}%` };
-  return <div className={styles.container} style={style} />;
+  return (
+    <div className={styles.progressContainer}>
+      <div className={styles.background} />
+      <div className={styles.container} style={style}>
+        <div
+          className={styles.progressIndicator}
+          onDrag={(e) => {
+            console.log("Draggaing", e.clientX);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -51,9 +51,7 @@ export default function VideoPlayer() {
     <div className={styles.container}>
       <video src="./test.mp4" className={styles.video} ref={videoRef} />
       <div className={styles.footer}>
-        <div className={styles.progressContainer}>
-          <Progress percentage={percentage} />
-        </div>
+        <Progress percentage={percentage} />
         <Controls onPlayChange={togglePlaying} isPlaying={isPlaying} />
       </div>
     </div>
