32c1e8fbe84dc1073cad74ce8c0226e6d1b1324c
Malik Elbadri <elbadrimalik@gmail.com>
2024-01-16 15:30:09 +0300

    Hide ambient on scroll

--- src/components/Ambient/index.tsx
+++ src/components/Ambient/index.tsx
@@ -1,26 +1,18 @@
 import styles from "./index.module.scss";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useVideo from "../hooks/useVideo";
-const canvas = document.createElement("canvas");
-const image = document.createElement("img");
+
 export default function Ambient({ enabled }: { enabled: boolean }) {
+  const [scrolling, setScrolling] = useState(false);
   const ambientRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { container, video } = useVideo();
-
-  const imageAppended = useRef(false);
 
   useEffect(() => {
     const ambient = ambientRef.current;
     const canvas = canvasRef.current;
 
     if (!enabled) return;
-
-    // if (!imageAppended.current) {
-    //   image.setAttribute("class", styles.ambientImage);
-    //   ambient?.appendChild(image);
-    //   imageAppended.current = true;
-    // }
 
     const interval = setInterval(() => {
       if (!ambient || !container || !canvas || !video) return;
@@ -38,17 +30,34 @@ export default function Ambient({ enabled }: { enabled: boolean }) {
       canvas
         .getContext("2d")
         ?.drawImage(video, 0, 0, video.videoWidth / 10, video.videoHeight / 10);
-
-      image.src = canvas.toDataURL();
-    }, 50);
+    }, 100);
 
     return () => {
       clearInterval(interval);
     };
   }, [container, enabled, video]);
 
+  useEffect(() => {
+    const handler = (e?: Event | boolean) => {
+      setScrolling(true);
+    };
+    const scrollEnd = () => {
+      setScrolling(false);
+    };
+    document.addEventListener("scroll", handler);
+    document.addEventListener("scrollend", scrollEnd);
+    return () => {
+      document.removeEventListener("scroll", handler);
+      document.removeEventListener("scrollend", scrollEnd);
+    };
+  }, []);
+
   return (
-    <div ref={ambientRef} className={styles.container}>
+    <div
+      ref={ambientRef}
+      className={styles.container}
+      data-scrolling={scrolling}
+    >
       <canvas ref={canvasRef} className={styles.ambientImage} />
     </div>
   );
